802e88ad703202e05556cf788696986dfa2deaed
Nicole <barrosnicole094@gmail.com>
2018-11-07 11:31:10 -0300

    remove conda.

--- gestao_cliente/gestao_cliente/settings.py
+++ gestao_cliente/gestao_cliente/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = [gestao_clientes2019.herokuapp.com]
+ALLOWED_HOSTS = ['gestao_clientes2019.herokuapp.com']
 
 
 # Application definition
