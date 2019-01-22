5adacdb08b824065c48ccad56500ee2c52903402
Nicole <barrosnicole094@gmail.com>
2019-01-21 23:49:35 -0300

    primeiro commit

--- gestao_cliente/gestao_cliente/settings.py
+++ gestao_cliente/gestao_cliente/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['gestao-cliente2019.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['gestaocliente2019.herokuapp.com', 'localhost']
 
 
 # Application definition
